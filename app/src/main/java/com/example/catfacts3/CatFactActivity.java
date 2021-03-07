d95ecb0a1dcf6cbba03805fd4d538f3eebff2712
Ian Chen <ichen2@lsu.edu>
2021-03-07 13:06:10 -0600

    Testing new auth

--- app/src/main/java/com/example/catfacts3/CatFactActivity.java
+++ app/src/main/java/com/example/catfacts3/CatFactActivity.java
@@ -22,11 +22,6 @@ public class CatFactActivity extends AppCompatActivity {
         String author = intent.getStringExtra("author");
         catFactText.setText(catFact);
         catFactAuthor.setText(getString(R.string.author_msg, author));
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        backButton.setOnClickListener(v -> finish());
     }
 }
